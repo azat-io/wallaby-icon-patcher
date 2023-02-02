4a9f272fe8b7aaecb07f7044f55ddc5f5b19b5f6
Azat S <to@azat.io>
2023-02-02 23:03:56 +0300

    chore: init

---
+++ patch.sh
@@ -0,0 +1,2 @@
+cp wallaby-icon.svg /Users/azat/.vscode/extensions/wallabyjs.wallaby-vscode-*/images/logo-outline.svg
+cp wallaby-icon.svg /Users/azat/.vscode/extensions/wallabyjs.quokka-vscode-*/images/logo-outline.svg
